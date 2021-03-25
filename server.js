92ff4a9617c974f41eb18dc8f4ca088411123ce8
Nicholas Kiddle <32177721+ngkiddle@users.noreply.github.com>
2021-03-25 02:43:08 -0700

    server index.js

--- server.js
+++ server.js
@@ -133,4 +133,12 @@ async function terminateThreads(){
     }
 }
 
+app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
